9cb75a439f4e5d29c165a8319799c1e7322410c3
iambasantarai <iambasantarai@gmail.com>
2024-12-05 23:28:18 +0545

    WIP

---
+++ background.js
@@ -0,0 +1,10 @@
+chrome.action.onClicked.addListener((tab) => {
+  chrome.scripting.executeScript({
+    target: { tabId: tab.id },
+    func: invertPageColors,
+  });
+});
+
+function invertPageColors() {
+  document.body.style.filter = "invert(100%) !important";
+}
